711dd881be29148a05798ba47a1584c2133ab903
Patrick <pwakooijman@gmail.com>
2021-02-20 11:51:18 +0100

    Completed module

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -1,12 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Tasker.Pages
 {
-    public class CreateTask : PageModel
+    public class CreateTaskModel : PageModel
     {
+        [BindProperty]
+        public Task NewTask { get; set; }
         public void OnGet()
         {
             
+        }
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage("Index");
         }
     }
 }
